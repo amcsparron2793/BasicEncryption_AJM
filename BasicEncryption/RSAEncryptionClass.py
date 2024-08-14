d0cfc74cc0605c5eb281bcd42d86df29f68b6268
amcsparron2793 <amcsparron2793@users.noreply.github.com>
2024-08-14 18:25:25 -0400

    fixed minor issues

--- BasicEncryption/RSAEncryptionClass.py
+++ BasicEncryption/RSAEncryptionClass.py
@@ -21,7 +21,10 @@ def aes_encrypt():
 
 class RSAKeyPairGenerator:
     def __init__(self, private_key_pass: str or None = None):
-        self.__private_key_pass = bytes(private_key_pass, 'utf-8')
+        if private_key_pass:
+            self.__private_key_pass = bytes(private_key_pass, 'utf-8')
+        else:
+            self.__private_key_pass = None
         self.private_key_ins = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048
@@ -56,32 +59,48 @@ class RSAKeyPairGenerator:
         private_key_pass = kwargs.get('private_key_pass', None)
         private_key_name = kwargs.get('private_key_name', 'private_key')
         public_key_name = kwargs.get('public_key_name', 'public_key')
+        overwrite_existing = kwargs.get('overwrite_existing', False)
+        output_path = Path(output_path)
 
         key_type = key_type.lower()
-        if Path(output_path).is_dir():
+        if output_path.is_dir():
             pass
         else:
-            raise NotADirectoryError("output_path does not exist, or is not a directory.")
+            try:
+                output_path.mkdir(exist_ok=True)
+            except FileNotFoundError:
+                raise NotADirectoryError("output_path parent does not exist.") from None
 
         if key_type not in self._valid_key_types:
             raise ValueError(f"{key_type} is not a valid option")
 
         if key_type == 'private':
-            with open(Path(output_path, f'{private_key_name}.pem'), 'w') as f:
-                f.write(self._gen_private_key(key_password=private_key_pass).decode())
+            private_full_path = Path(output_path, f'{private_key_name}.pem')
+            if (private_full_path.is_file() and overwrite_existing) or not private_full_path.is_file():
+                with open(private_full_path, 'w') as f:
+                    f.write(self._gen_private_key(key_password=private_key_pass).decode())
+            else:
+                raise FileExistsError("file already exists, use overwrite_existing=True to overwrite.")
 
         elif key_type == 'public':
-           with open(Path(output_path, f'{public_key_name}.pub'), 'w') as f:
-                f.write(self._gen_public_key().decode())
+           public_full_path = Path(output_path, f'{public_key_name}.pub')
+           if (public_full_path.is_file() and overwrite_existing) or not public_full_path.is_file():
+               with open(public_full_path, 'w') as f:
+                    f.write(self._gen_public_key().decode())
+           else:
+               raise FileExistsError("file already exists, use overwrite_existing=True to overwrite.")
+
 
         print(f"Wrote key to {f.name}")
         return f.name
 
-    def gen_keypair(self, private_key_pass=None, **kwargs):
+    def gen_keypair(self, private_key_pass=None, **kwargs) -> (str, str):
         public_key_output_location = kwargs.get('public_key_output_location', 'C:\\Users\\amcsparron\\Desktop\\')
         private_key_output_location = kwargs.get('private_key_output_location', 'C:\\Users\\amcsparron\\Desktop\\')
-        private_key_name = kwargs.get('private_key_name', None)
-        public_key_name = kwargs.get('public_key_name', None)
+        private_key_name = kwargs.get('private_key_name', 'private_key')
+        public_key_name = kwargs.get('public_key_name', 'public_key')
+        overwrite_existing = kwargs.get('overwrite_existing', False)
+
         if private_key_pass is None:
             private_key_pass = self.__private_key_pass
         else:
@@ -90,10 +109,12 @@ class RSAKeyPairGenerator:
         private_key_location = self._write_key(output_path=private_key_output_location,
                                                key_type='private',
                                                private_key_pass=private_key_pass,
-                                               private_key_name=private_key_name)
+                                               private_key_name=private_key_name,
+                                               overwrite_existing=overwrite_existing)
         public_key_location = self._write_key(output_path=public_key_output_location,
                                               key_type='public',
-                                              public_key_pass=public_key_name)
+                                              public_key_pass=public_key_name,
+                                              overwrite_existing=overwrite_existing)
         return private_key_location, public_key_location
 
 
@@ -103,6 +124,9 @@ class RSAEncrypterDecrypter:
         #  into properties and check for correct extensions/existence
         self._private_key_location = kwargs.get('private_key_location', None)
         self._public_key_location = kwargs.get('public_key_location', None)
+        self.__private_key_pass = kwargs.get('private_key_pass', None)
+
+
         if self._private_key_location is None and self._public_key_location is None:
             raise AttributeError("both public and private key location cannot be none")
 
@@ -122,7 +146,7 @@ class RSAEncrypterDecrypter:
         self._serialized_public_key = None
 
         if self._private_key_location:
-            self.__private_key = self._load_key('private', private_key_pass='password')
+            self.__private_key = self._load_key('private', private_key_pass=self.__private_key_pass)
         if self._public_key_location:
             self._public_key = self._load_key('public')
 
@@ -237,11 +261,18 @@ class RSAEncrypterDecrypter:
         elif not return_as_bytes:
             return decrypted_bytes.decode('utf-8')
 
+if __name__ == '__main__':
+    GenKey = RSAKeyPairGenerator()
+    """private_key_path, public_key_path = GenKey.gen_keypair(public_key_output_location='../Misc_Project_Files/keys',
+                                                           private_key_output_location='../Misc_Project_Files/keys',
+                                                           private_key_pass='password', overwrite_existing=False)"""
+    key_dir = Path('../Misc_Project_Files/keys')
+    EnDe = RSAEncrypterDecrypter(private_key_location=key_dir.joinpath('private_key.pem'),
+                                 public_key_location=key_dir.joinpath('public_key.pub'),
+                                 private_key_pass='password')
 
-EnDe = RSAEncrypterDecrypter(private_key_location='C:\\Users\\amcsparron\\Desktop\\ex_private_key.pem',
-                             public_key_location='C:\\Users\\amcsparron\\Desktop\\ex_public_key.pub')
-"""with open('C:\\Users\\amcsparron\\Desktop\\stick.png', 'rb') as f:
-    b = f.read()
-
-enc = EnDe.encrypt(b)
-print(type(enc))"""
+    """with open('C:\\Users\\amcsparron\\Desktop\\stick.png', 'rb') as f:
+        b = f.read()
+    
+    enc = EnDe.encrypt(b)
+    print(type(enc))"""
